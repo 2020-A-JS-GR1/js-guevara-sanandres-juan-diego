33c417eab375a5318e2b56df28c1793bff5d3027
Juan Diego Guevara <junt_sm_@hotmail.com>
2020-06-12 17:19:17 -0500

    Tipos de Variables

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -1,10 +1,23 @@
-//Mutables and Inmutables
+//Mutable
 
-var number_one = 1;
+let number_one = 1;
 let number_two = 2;
 
 number_one = 5;
 number_two = 6;
 number_one = true;
 number_two = false;
-
+//Immutable
+const file_configuration = "PDF";
+//file_configuration = "XML";
+//Variables Types
+const number = 4; //number
+const text = "COVID"; //string
+const bool = "False"; //boolean
+const love = null; //object
+const life = undefined; //undefined
+console.log(typeof number);
+console.log(typeof text);
+console.log(typeof bool);
+console.log(typeof love);
+console.log(typeof life);
