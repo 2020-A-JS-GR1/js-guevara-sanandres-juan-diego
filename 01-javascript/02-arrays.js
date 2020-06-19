092efbdbe960dfefc5239d52fe34297730a08f37
Juan Diego Guevara <junt_sm_@hotmail.com>
2020-06-19 16:55:39 -0500

    Arreglos, introducción operadores

--- 01-javascript/02-arrays.js
+++ 01-javascript/02-arrays.js
@@ -1,4 +1,4 @@
-const array = [1,2,3,4,5,6,7,8,9,10];
+const array = [5,6,7,8,9,10];
 //array = 1;
 let anything = "ABDP";
 anything = false;
@@ -8,3 +8,36 @@ anything = null;
 const whole_array = [
   true, 4, 4.2, "Juan", undefined, null, {}, [0,1,false]
 ];
+
+// for of
+for (let number of array){
+  console.log('number', number);
+}
+
+// for in
+for (let number in array){
+  console.log('number', number);
+}
+// add element at the end
+array.push(13);
+
+//remove last element
+array.pop();
+
+// add element at startup
+array.unshift(4);
+
+console.log(array);
+
+//insert or elminate an element
+array.splice(0,3);
+console.log(array);
+array.splice(0,0,10,11,7);
+console.log(array);
+
+//Get element index
+const index = array.indexOf(9);
+console.log('indice', index);
+
+array.splice(array.indexOf(9),1);
+
