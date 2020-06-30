494de83b838db4c83678de2829ccf4905bfd8019
Juan Diego Guevara <junt_sm_@hotmail.com>
2020-06-30 17:19:56 -0500

    Ejercicio Terminado

--- 01-javascript/07-ejercicio-lectura-escritura.js
+++ 01-javascript/07-ejercicio-lectura-escritura.js
@@ -1,1 +1,31 @@
 const fs = require('fs');
+/*
+Hacer una función que me acepte como parametro una variable
+con el path del archivo y con el contenido que se va agregar
+al archivo. La función toma 2 parámetros y debe leer el archivo
+y añadir el texo al final
+*/
+
+function write_File(path, fileContent) {
+    fs.readFile(
+        path,
+        'utf-8',
+        (error, content) => {
+            error ? console.log(error) : fs.writeFile(
+                path,
+                content + '\n' +fileContent,
+                'utf-8',
+                (error) => {error ? console.log(error) : console.log('Escritura completada');},
+            );
+        },
+    );
+    /*fs.appendFile(
+        path,
+        fileContent,
+        'utf-8',
+        (error) => {error ? console.log(error) : console.log('Escritura completada');},
+    );
+    */
+}
+
+write_File('./06-ejemplo.txt','Lo he conseguido x4');
