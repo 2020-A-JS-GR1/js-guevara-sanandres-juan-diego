1c731fcda57d9360d0898aec5510b5a27668b2da
Juan Diego Guevara <junt_sm_@hotmail.com>
2020-07-07 18:04:18 -0500

    Ejercicio final inquirer

--- 01-javascript/11-inquirer.js
+++ 01-javascript/11-inquirer.js
@@ -1,19 +1,23 @@
 const inquirer = require('inquirer');
 
 async function main (){
-    const response = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'apellido',
-            message: 'Ingresa tu nombre',
-        },
-        {
-            type: 'input',
-            name: 'edad',
-            message: 'Ingresa tu edad',
-        }
-    ]);
-    console.log('Respuesta', response)
+    try{
+        const response = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'apellido',
+                message: 'Ingresa tu nombre',
+            },
+            {
+                type: 'input',
+                name: 'edad',
+                message: 'Ingresa tu edad',
+            }
+        ]);
+        console.log('Respuesta', response)
+    }catch (e) {
+        console.error(e);
+    }
 }
 
 main();
